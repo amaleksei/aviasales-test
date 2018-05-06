7a81ce61899be2499b4dc89ce4349becd8f0c6d8
amaleksei <alekseizhigzhitov@gmail.com>
2018-05-06 19:57:40 +0300

    Draft filter

--- src/javascripts/index.jsx
+++ src/javascripts/index.jsx
@@ -7,20 +7,197 @@ import Tickets from './tickets/tickets.jsx';
 
 const dataSorted = dataJSON.tickets.sort((a, b) => a.price - b.price);
 
-class Hello extends React.Component {
+// class FlavorForm extends React.Component {
+//   state = { value: '' };
+//
+//   handleChange = (e) => {
+//     this.setState({ value: e.target.value });
+//   };
+//
+//   handleSubmit = (e) => {
+//     e.preventDefault();
+//     alert(`Your favorite flavor is: ${this.state.value}`);
+//   };
+//
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <label>
+//           Pick your favorite La Croix flavor:
+//           <select value={this.state.value} onChange={this.handleChange}>
+//             <option value="grapefruit">Grapefruit</option>
+//             <option value="lime">Lime</option>
+//             <option value="coconut">Coconut</option>
+//             <option value="mango">Mango</option>
+//           </select>
+//         </label>
+//         <input type="submit" value="Submit" />
+//       </form>
+//     );
+//   }
+// }
+
+class Filter extends React.Component {
+  state = { value: '' };
+
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Your favorite flavor is: ${this.state.value}`);
+  };
+
   render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>Количество пересадок</label>
+      </form>
+    );
+  }
+}
+
+// class ListItems extends React.Component {
+//   render() {
+//     const numbers = [0, 1];
+//     return (
+//       numbers.map(numbers => numbers)
+//     );
+//   }
+// }
+
+const dataFiltred = () => {
+  const numbers = [0, 1];
+
+  // const all =
+
+  const number = (obj) => {
+    return obj === 0 || obj === 2;
+  };
+
+  const filterByStops = (item) => {
+    if (number(item.stops)) {
+      return true;
+    }
+    return false;
+  };
+
+  // const listItems = numbers.map(numbers => {
+  //   return (
+  //     item.stops === numbers
+  //   );
+  // });
+  return dataSorted.filter(filterByStops);
+};
+
+class App extends React.Component {
+  static defaultProps = {
+    all: true,
+  };
+
+  constructor(props) {
+    super(props); // всегда обязательно
+    this.state = {
+      all: this.props.all,
+      noStops: true,
+      oneStops: true,
+      twoStops: true,
+      threeStops: true,
+    };
+  }
+
+
+  // this.setState({ isShown: !this.state.isShown });
+
+  toggle(event) {
+    const name = event.target.name;
+    this.setState({
+      all: !this.state.all,
+      noStops: !this.state.noStops,
+    });
+  }
+
+  onChangeAll = (e) => {
+
+  };
+
+  // handleChange = (event) => {
+  //   const name = event.target.name;
+  //   this.setState({
+  //     [name]: event.target.checked,
+  //   });
+  // };
+
+  // handleRemove = (value) => (e) => {
+  //   e.preventDefault();
+  //   const newItems = this.state.stops.filter(item => item !== value);
+  //   this.setState({ stops: newStops });
+  // };
+
+  // {this.state.stops.map(i =>
+  // <Item onRemove={this.handleRemove} value={i} />
+  // )}
+
+  // isAll = {
+  //   if({this.state.isAll}) {
+  //     item
+  //   }
+  //   item.stops === 0
+  // };
+  render() {
+    const isCheked = [];
+    isCheked.push(
+      //     <p key={uniqueId()}>{this.state.all ? 'Checked' : 'Unchecked'}</p>,
+      <p key={uniqueId()}>{this.state.noStops ? 'Checked' : 'Unchecked'}</p>);
+    const checkboxisAll = (<span><input name="all" type="checkbox" onClick={this.toggle.bind(this)} /><label>Все</label></span>);
+    const checkboxNoStops = (<span><input name="noStops" type="checkbox" onChange={this.toggle.bind(this)} /><label>Без пересадок {this.state.noStops}</label></span>);
     return (
       <div>
         <div className="hello">
           <span>Hello webpack!</span>
+          <div className="FilterData">
+            {checkboxisAll}
+            <br />
+            {checkboxNoStops}
+            {/* <IsCheked data={!this.state.noStops}/> */}
+            <br />
+            <label>
+              <input
+                name="oneStops"
+                type="checkbox"
+                onChange={this.handleChange}
+              />
+              <span>1 пересадка</span>
+            </label>
+            <br />
+            <label>
+              <input
+                name="twoStops"
+                type="checkbox"
+                onChange={this.handleChange}
+              />
+              <span>2 пересадки</span>
+            </label>
+            <br />
+            <label>
+              <input
+                name="threeStops"
+                type="checkbox"
+                onChange={this.handleChange}
+              />
+              <span>3 пересадки</span>
+            </label>
+          </div>
         </div>
-        <Tickets data={dataSorted} />
+        <Filter />
+        <Tickets data={dataFiltred()} />
       </div>
     );
   }
 }
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 if (module.hot) {
   module.hot.accept();
--- src/javascripts/tickets/tickets.jsx
+++ src/javascripts/tickets/tickets.jsx
@@ -6,10 +6,12 @@ const flightDetail = (item, value) => {
     <h2 key={uniqueId()}>{item.departure_time}</h2>,
     <span key={uniqueId()}>{item.origin}, {item.origin_name}</span>,
     <p key={uniqueId()}>{item.departure_date}</p>,
+    <h1 key={uniqueId()}>{item.price}</h1>,
   ] : [
     <h2>{item.arrival_time}</h2>,
     <span>{item.destination}, {item.destination_name}</span>,
     <p>{item.arrival_date}</p>,
+    <h1 key={uniqueId()}>{item.price}</h1>,
   ];
 
   return (
@@ -18,6 +20,22 @@ const flightDetail = (item, value) => {
     </div>
   );
 };
+
+// [item.stops === thi]
+
+// class FilterOptions extends React.Component {
+//   render() {
+//     return {this.props.items.map(number => <Item value={number} />)};
+//   }
+// }
+//
+// class Item extends React.Component {
+//   render() {
+//     return item.stops === {this.props.value};
+//   }
+// }
+
+// {data.filter(item => {FilterOptions items={[1]}}).map(item => <div key={uniqueId()}>{flightDetail(item, 'departure')}</div>)}
 
 class Tickets extends React.Component {
   render() {
